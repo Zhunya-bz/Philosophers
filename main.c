c54fe61d573c8ff38f6bff1091f2734162d7f8ac
Zhunya-bz <fluffylion2013@gmail.com>
2021-07-28 21:44:30 +0300

    dead yeap

--- main.c
+++ main.c
@@ -41,6 +41,33 @@ void my_usleep(t_data *data, unsigned long long var, unsigned long long start)
 //	pthread_mutex_unlock(&data->next_p->mut);
 }
 
+void phil_eating(t_philo	philo)
+{
+	if (philo.next_d->dead == 1)
+		return ;
+	printf("%lu: %d is eating\n", get_time_0(philo.next_d),
+		   philo.name);
+	philo.last_eat = get_time_0(philo.next_d);
+	my_usleep(philo.next_d, philo.next_d->time_eat, get_time_0(philo.next_d));
+}
+
+void phil_sleeping(t_philo	philo)
+{
+	if (philo.next_d->dead == 1)
+		return ;
+	printf("%lu: %d is sleeping\n",
+		   get_time_0(philo.next_d), philo.name);
+	my_usleep(philo.next_d, philo.next_d->time_sleep, get_time_0(philo.next_d));
+}
+
+void phil_thinking(t_philo	philo)
+{
+	if (philo.next_d->dead == 1)
+		return ;
+	printf("%lu: %d is thinking\n",
+		   get_time_0(philo.next_d), philo.name);
+}
+
 void *philosoph(void *tmp)
 {
 	t_philo	philo;
@@ -64,18 +91,11 @@ void *philosoph(void *tmp)
 		pthread_mutex_lock(philo.right_fork);
 		printf("%lu: %d has taken a right fork\n", get_time_0(philo.next_d),
 			   philo.name);
-		printf("%lu: %d is eating\n", get_time_0(philo.next_d),
-			   philo.name);
-		philo.last_eat = get_time_0(philo.next_d);
-//		printf("LAST: %llu\n", philo.last_eat);
-		my_usleep(philo.next_d, philo.next_d->time_eat, get_time_0(philo.next_d));
+		phil_eating(philo);
 		pthread_mutex_unlock(philo.left_fork);
 		pthread_mutex_unlock(philo.right_fork);
-		printf("%lu: %d is sleeping\n",
-			   get_time_0(philo.next_d), philo.name);
-		my_usleep(philo.next_d, philo.next_d->time_sleep, get_time_0(philo.next_d));
-		printf("%lu: %d is thinking\n",
-			   get_time_0(philo.next_d), philo.name);
+		phil_sleeping(philo);
+		phil_thinking(philo);
 	}
 
 //	pthread_mutex_unlock(&mutex);
